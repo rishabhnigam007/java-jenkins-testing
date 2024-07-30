865da7c0198ce86e0b3db1ce5e4c337ed80851c6
Rishabh Nigam <rishabhakans@gmail.com>
2024-07-30 21:37:03 +0530

    spring mvc war project

--- Main.java
+++
@@ -1,8 +0,0 @@
-import java.util.Date;
-
-public class Main{
-    public static void main(String[] args) {
-        System.out.println("hii");
-        System.out.println("Add one more line");
-    }
-}
